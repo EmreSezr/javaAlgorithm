60e8bef8159a1964aa0ab323f45d5ab2b67a9aa1
Emre Sezer <1997.emresezer@gmail.com>
2023-06-14 17:01:42 +0300

    Word Length

---
+++ src/main/java/org/emresezer/soru6/WordLength.java
@@ -0,0 +1,17 @@
+package org.emresezer.soru6;
+
+import java.util.Scanner;
+
+public class WordLength {
+    // Bir metindeki harf sayısını bulan bir algoritma yazın.
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Cümle veya kelime yazınız(Boşluklar dahildir) :  ");
+        String cumle = scanner.nextLine();
+        char[] ch = new char[cumle.length()];
+        int kelimeSayac = 0;
+        //if(ch.is)
+        System.out.println("Karakter toplamı : " + ch.length);
+
+    }
+}
