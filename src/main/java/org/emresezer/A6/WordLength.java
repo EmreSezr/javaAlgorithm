8188879acb1c745d1168b607e9f07e9c348cab42
Emre Sezer <1997.emresezer@gmail.com>
2023-06-15 23:20:39 +0300

    Word length calculate

--- src/main/java/org/emresezer/A6/WordLength.java
+++ src/main/java/org/emresezer/A6/WordLength.java
@@ -3,15 +3,18 @@ package org.emresezer.A6;
 import java.util.Scanner;
 
 public class WordLength {
-    // Bir metindeki harf sayısını bulan bir algori tma yazın.
+    // Bir metindeki harf sayısını bulan bir algoritma yazın.
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Cümle veya kelime yazınız(Boşluklar dahildir) :  ");
         String cumle = scanner.nextLine();
-        char[] ch = new char[cumle.length()];
-        int kelimeSayac = 0;
-        //if(ch.is)
-        System.out.println("Karakter toplamı : " + ch.length);
+        cumle = cumle.trim(); // Baştaki ve sondaki boşlukları kaldırma
+
+        int kelimeSayac = cumle.length() - cumle.replace(" ", "").length();
+        int karakterSayac = cumle.length() - kelimeSayac;
+
+        System.out.println("Karakter toplamı : " + karakterSayac);
+        System.out.println("Kelime sayısı   : " + (kelimeSayac + 1));
 
     }
 }
