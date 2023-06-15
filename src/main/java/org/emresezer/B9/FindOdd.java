30b6e8022efc71cfea0f87c566cbb9e41dbb108a
Emre Sezer <1997.emresezer@gmail.com>
2023-06-15 22:32:09 +0300

    Find even numbers in random series

---
+++ src/main/java/org/emresezer/B9/FindOdd.java
@@ -0,0 +1,31 @@
+package org.emresezer.B9;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class FindOdd {
+    public static void main(String[] args) {
+        //
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        System.out.print("Dizi Kaç Elemanlı Olsun : ");
+        int listCount = scanner.nextInt();
+        int[] list = new int[listCount];
+
+        for (int i = 0; i < listCount; i++) {
+            list[i] = random.nextInt(1000);
+        }
+        System.out.println(Arrays.toString(list));
+
+        System.out.println("Dizideki Tek Sayılar : ");
+        for (int numbers : list) {
+            if (!(numbers % 2 == 0)) {
+                System.out.print(numbers + " ");
+            }
+        }
+
+
+        scanner.close();
+    }
+}
