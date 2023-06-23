e82f4850b6b40a2442fcc3fcb30ddd1f85c22637
Emre Sezer <1997.emresezer@gmail.com>
2023-06-23 03:57:26 +0300

    Implement a program to replace question marks in an input string with provided substrings

---
+++ src/main/java/org/emresezer/C9/FillTheQuestionMark.java
@@ -0,0 +1,62 @@
+package org.emresezer.C9;
+import java.util.Scanner;
+
+public class FillTheQuestionMark {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Input: ");
+        String input = scanner.nextLine();
+
+        System.out.print("Substring: ");
+        String substring = scanner.nextLine();
+
+        int questionMarkCount = countQuestionMarks(input);
+        int substringCount = countSubstring(substring);
+
+        if (questionMarkCount < substringCount) {
+            System.out.println("-1");
+        } else {
+            String output = fillQuestionMarks(input, substring);
+            System.out.println("Output: " + output);
+        }
+    }
+
+    private static int countQuestionMarks(String input) {
+        int count = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == '?') {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private static int countSubstring(String substring) {
+        int count = 0;
+        for (int i = 0; i < substring.length(); i++) {
+            if (substring.charAt(i) != ' ') {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private static String fillQuestionMarks(String input, String substring) {
+        StringBuilder output = new StringBuilder(input);
+        int subIndex = 0;
+
+        for (int i = 0; i < output.length(); i++) {
+            if (output.charAt(i) == '?') {
+                if (subIndex < substring.length()) {
+                    output.setCharAt(i, substring.charAt(subIndex));
+                    subIndex++;
+                } else {
+                    output.setCharAt(i, '-');
+                }
+            }
+        }
+
+        return output.toString();
+    }
+}
