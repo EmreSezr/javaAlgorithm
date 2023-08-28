e2ca7e720691ba684a742874792cd590236cffaf
emre <emre@example.com>
2023-08-29 02:06:06 +0300

    will improve rotate array method

---
+++ src/main/java/org/emresezer/D9/RotateArray.java
@@ -0,0 +1,32 @@
+package org.emresezer.D9;
+
+/**
+ * Input: nums = [1,2,3,4,5,6,7], k = 3
+ * Output: [5,6,7,1,2,3,4]
+ * Explanation:
+ * rotate 1 steps to the right: [7,1,2,3,4,5,6]
+ * rotate 2 steps to the right: [6,7,1,2,3,4,5]
+ * rotate 3 steps to the right: [5,6,7,1,2,3,4]
+ * <p>
+ * <p>
+ * Input: nums = [-1,-100,3,99], k = 2
+ * Output: [3,99,-1,-100]
+ * Explanation:
+ * rotate 1 steps to the right: [99,-1,-100,3]
+ * rotate 2 steps to the right: [3,99,-1,-100]
+ */
+public class RotateArray {
+    public void rotate(int[] nums, int k) {
+        for (int i = 0; i < nums.length; i++) {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+        //expected output : [5,6,7,1,2,3,4]
+        RotateArray rotateArray = new RotateArray();
+        rotateArray.rotate(nums, 2);
+    }
+}
